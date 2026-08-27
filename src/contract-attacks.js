fd161f4da065fbe0090c89401dbf1e52ec5dd846
AidaMichel <126730924+AidaMichel@users.noreply.github.com>
2026-08-27 17:30:09 +0400

    fix: bind capture to returned generator output

--- src/contract-attacks.js
+++ src/contract-attacks.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const {
+  types: utilTypes
+} = require("node:util");
+
+const {
   buildExperiment,
   captureGeneratorOutputForActiveExperiment,
   createExperimentCapture,
@@ -8,6 +12,7 @@ const {
 } = require("./contract-experiment-safe");
 
 const {
+  authorizeGeneratorOutput,
   withSnapshotCaptureListener
 } = require("./contract-experiment-hook");
 
@@ -18,12 +23,98 @@ const {
 
 const defineProperty =
   Object.defineProperty;
+const getOwnPropertyDescriptor =
+  Object.getOwnPropertyDescriptor;
+const getOwnPropertyDescriptors =
+  Object.getOwnPropertyDescriptors;
+const getPrototypeOf =
+  Object.getPrototypeOf;
+const objectCreate =
+  Object.create;
+const reflectApply =
+  Reflect.apply;
+const isProxy =
+  utilTypes.isProxy;
+
+function makeCoreOptions(options) {
+  if (
+    options === null ||
+    typeof options !== "object" ||
+    isProxy(options)
+  ) {
+    return options;
+  }
+
+  const generatorDescriptor =
+    getOwnPropertyDescriptor(
+      options,
+      "generator"
+    );
+
+  if (
+    generatorDescriptor === undefined ||
+    !(
+      "value" in generatorDescriptor
+    ) ||
+    typeof generatorDescriptor.value !==
+      "function"
+  ) {
+    return options;
+  }
+
+  const originalGenerator =
+    generatorDescriptor.value;
+  const descriptors =
+    getOwnPropertyDescriptors(options);
+
+  defineProperty(
+    descriptors,
+    "generator",
+    {
+      value: {
+        value:
+          function authorizedGenerator(
+            ...args
+          ) {
+            const rawOutput =
+              reflectApply(
+                originalGenerator,
+                this,
+                args
+              );
+
+            authorizeGeneratorOutput(
+              rawOutput
+            );
+
+            return rawOutput;
+          },
+        writable:
+          generatorDescriptor.writable,
+        enumerable:
+          generatorDescriptor.enumerable,
+        configurable:
+          generatorDescriptor.configurable
+      },
+      writable: true,
+      enumerable: true,
+      configurable: true
+    }
+  );
+
+  return objectCreate(
+    getPrototypeOf(options),
+    descriptors
+  );
+}
 
 async function runContractAttacks(
   options = {}
 ) {
   const experimentCapture =
     createExperimentCapture(options);
+  const coreOptions =
+    makeCoreOptions(options);
 
   const result =
     await withExperimentCapture(
@@ -33,7 +124,7 @@ async function runContractAttacks(
           captureGeneratorOutputForActiveExperiment,
           () =>
             runContractAttacksCore(
-              options
+              coreOptions
             )
         )
     );
