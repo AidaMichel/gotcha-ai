a1c5a0e4a577a92f61522e660e24635cae07ff61
AidaMichel <126730924+AidaMichel@users.noreply.github.com>
2026-09-05 00:29:42 +0400

    qa: probe direct util types loader across runtimes

--- scripts/qa/m13-round9-node18-diagnostic.js
+++ scripts/qa/m13-round9-node18-diagnostic.js
@@ -31,12 +31,11 @@ run("util.inspect getter trace", `
     enumerable: descriptor.enumerable,
     get() {
       calls += 1;
-      try { process.stderr.write("INSPECT_GET\\n" + new Error("inspect access").stack + "\\n"); } catch {}
       throw new Error("inspect getter executed");
     }
   });
   try { require(${JSON.stringify(runtimePath)}); }
-  catch (error) { try { process.stderr.write("RUNTIME_ERROR\\n" + (error && error.stack || error) + "\\n"); } catch {} }
+  catch {}
   finally { Object.defineProperty(util, "inspect", descriptor); }
   process.stdout.write("inspectCalls=" + calls + "\\n");
 `);
@@ -55,7 +54,6 @@ run("util.types getter trace", `
     enumerable: descriptor.enumerable,
     get() {
       calls += 1;
-      try { process.stderr.write("TYPES_GET\\n" + new Error("types access").stack + "\\n"); } catch {}
       throw new Error("types getter executed");
     }
   });
@@ -63,10 +61,37 @@ run("util.types getter trace", `
     const api = require(${JSON.stringify(indexPath)});
     void api.createStructuredProviderAdapter;
     void api.runContractAttacks;
-  } catch (error) {
-    try { process.stderr.write("INDEX_ERROR\\n" + (error && error.stack || error) + "\\n"); } catch {}
-  } finally {
+  } catch {}
+  finally {
     Object.defineProperty(util, "types", descriptor);
   }
   process.stdout.write("typesCalls=" + calls + "\\n");
+`);
+
+run("direct node:util/types under poisoned util.types", `
+  "use strict";
+  const util = require("node:util");
+  const descriptor = Object.getOwnPropertyDescriptor(util, "types");
+  if (!descriptor || descriptor.configurable !== true) {
+    process.stdout.write("directTypes descriptor not configurable\\n");
+    process.exit(0);
+  }
+  let calls = 0;
+  Object.defineProperty(util, "types", {
+    configurable: true,
+    enumerable: descriptor.enumerable,
+    get() {
+      calls += 1;
+      throw new Error("types getter executed");
+    }
+  });
+  let loaded = false;
+  try {
+    const types = require("node:util/types");
+    loaded = types !== null && typeof types === "object";
+  } catch {}
+  finally {
+    Object.defineProperty(util, "types", descriptor);
+  }
+  process.stdout.write("directTypesCalls=" + calls + " loaded=" + loaded + "\\n");
 `);
